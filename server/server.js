59c97da58e52fb85fa3177bd447564011185dbfb
DavidOde100 <david.odejimi677@gmail.com>
2026-07-01 19:27:33 -0500

    feat: set up Express server and PostgreSQL database connection with dotenv configuration

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+
+// create express app
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+	res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`)
+})
